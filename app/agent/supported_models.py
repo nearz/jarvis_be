7a846258d3c1abe8a706b63f8006379f72efa222
nearz <andrewjneary@gmail.com>
2025-11-07 06:14:17 -0500

    updated raw model config params

--- app/agent/supported_models.py
+++ app/agent/supported_models.py
@@ -26,43 +26,43 @@ def validate_models(models: dict) -> dict[str, ModelConfig]:
 
 supported_models_raw = {
     "gpt-4o": {
-        "provider_string": "openai",
-        "model_string": "gpt-4o",
+        "provider": "openai",
+        "model": "gpt-4o",
         "tool_support": True,
     },
     "gpt-4o-mini": {
-        "provider_string": "openai",
-        "model_string": "gpt-4o-mini",
+        "provider": "openai",
+        "model": "gpt-4o-mini",
         "tool_support": True,
     },
     "gpt-4.1": {
-        "provider_string": "openai",
-        "model_string": "gpt-4.1",
+        "provider": "openai",
+        "model": "gpt-4.1",
         "tool_support": True,
     },
     "gpt-4.1-mini": {
-        "provider_string": "openai",
-        "model_string": "gpt-4.1-mini",
+        "provider": "openai",
+        "model": "gpt-4.1-mini",
         "tool_support": True,
     },
     "gpt-4.1-nano": {
-        "provider_string": "openai",
-        "model_string": "gpt-4.1-nano",
+        "provider": "openai",
+        "model": "gpt-4.1-nano",
         "tool_support": True,
     },
     "claude-opus-4-1": {
-        "provider_string": "anthropic",
-        "model_string": "claude-opus-4-1",
+        "provider": "anthropic",
+        "model": "claude-opus-4-1",
         "tool_support": True,
     },
     "claude-haiku-4-5": {
-        "provider_string": "anthropic",
-        "model_string": "claude-haiku-4-5",
+        "provider": "anthropic",
+        "model": "claude-haiku-4-5",
         "tool_support": True,
     },
     "claude-sonnet-4-5": {
-        "provider_string": "anthropic",
-        "model_string": "claude-sonnet-4-5",
+        "provider": "anthropic",
+        "model": "claude-sonnet-4-5",
         "tool_support": True,
     },
 }
